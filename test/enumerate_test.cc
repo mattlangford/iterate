fd9805876ca3d1a2d2d0ac7be4d95230b123cfde
Matt Langford <matthew.langford95@gmail.com>
2021-01-26 12:04:08 -0500

    Util functions for ease of use

--- test/enumerate_test.cc
+++ test/enumerate_test.cc
@@ -2,18 +2,23 @@
 
 #include <gtest/gtest.h>
 
+#include <map>
 #include <vector>
 
+namespace {
 struct TestType {
     size_t i;
-    ~TestType() { i = -1; }  // reset I if this gets deconstructed
+    ~TestType() { i = -1; }  // reset if this gets deconstructed
 };
 
 template <typename T>
 constexpr bool kIsConstRef = std::is_const_v<std::remove_reference_t<T>>;
 
+std::vector<TestType> test_type_vector() { return {{0}, {1}, {2}}; }
+}  // namespace
+
 TEST(Enumerate, vector_reference) {
-    std::vector<TestType> input{{0}, {1}, {2}};
+    auto input = test_type_vector();
     for (auto [i, test_type] : it::enumerate(input)) {
         EXPECT_FALSE(kIsConstRef<decltype(test_type)>);
         EXPECT_EQ(i, test_type.i);
@@ -21,7 +26,7 @@ TEST(Enumerate, vector_reference) {
 }
 
 TEST(Enumerate, vector_const_reference) {
-    const std::vector<TestType> input{{0}, {1}, {2}};
+    const auto input = test_type_vector();
     for (auto [i, test_type] : it::enumerate(input)) {
         EXPECT_TRUE(kIsConstRef<decltype(test_type)>);
         EXPECT_EQ(i, test_type.i);
@@ -29,14 +34,14 @@ TEST(Enumerate, vector_const_reference) {
 }
 
 TEST(Enumerate, vector_rvalue_reference) {
-    for (auto [i, test_type] : it::enumerate(std::vector<TestType>{{0}, {1}, {2}})) {
+    for (auto [i, test_type] : it::enumerate(test_type_vector())) {
         EXPECT_FALSE(kIsConstRef<decltype(test_type)>);
         EXPECT_EQ(i, test_type.i);
     }
 }
 
 TEST(Enumerate, vector_copy) {
-    std::vector<TestType> input{{0}, {1}, {2}};
+    auto input = test_type_vector();
 
     for (auto [i, test_type] : it::enumerate_copy(input)) {
         EXPECT_FALSE(kIsConstRef<decltype(test_type)>);
@@ -48,5 +53,19 @@ TEST(Enumerate, vector_copy) {
     for (auto [i, test_type] : it::enumerate(input)) {
         EXPECT_FALSE(kIsConstRef<decltype(test_type)>);
         EXPECT_EQ(i, test_type.i);
+    }
+}
+
+TEST(Enumerate, map_reference) {
+    auto input = test_type_vector();
+    std::map<int, TestType> map;
+    for (auto [i, test_type] : it::enumerate(input)) map[i + 100] = test_type;
+
+    for (auto [i, item] : it::enumerate(map)) {
+        EXPECT_FALSE(kIsConstRef<decltype(item)>);
+
+        const auto& [key, value] = item;
+        EXPECT_EQ(i, value.i);
+        EXPECT_EQ(i + 100, key);
     }
 }
