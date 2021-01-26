05c917ca31ab11500b1251454c385d336bc49495
Matt Langford <matthew.langford95@gmail.com>
2021-01-26 14:58:23 -0500

    Handle const and things

--- enumerate.hh
+++ enumerate.hh
@@ -16,24 +16,54 @@ struct EnumerateIterator {
         return *this;
     }
 
+    using value_type = std::pair<size_t, ReferenceType<It>>;
+    using reference = value_type;
+
     // NOTE: This may return a const or mutable reference depending on the constness of the iterator
-    std::pair<size_t, ReferenceType<It>> operator*() const { return {index, *element}; }
+    reference operator*() const { return {index, *element}; }
+
     bool operator!=(const EnumerateIterator &rhs) const { return element != rhs.element; }
+    bool operator<(const EnumerateIterator &rhs) const { return element < rhs.element; }
 };
 
+/// Accepts by value, reference, or rvalue reference
 template <typename Container>
 struct Enumerate {
-    using iterator = EnumerateIterator<IteratorType<Container>>;
-    iterator begin() const { return iterator{0, container.begin()}; }
-    iterator end() const { return iterator{0, container.end()}; }
-    Container &container;
-};
-template <typename Container>
-struct EnumerateCopy {
-    using iterator = EnumerateIterator<IteratorType<Container>>;
-    iterator begin() { return iterator{0, container.begin()}; }
-    iterator end() { return iterator{0, container.end()}; }
+    using iterator = EnumerateIterator<typename std::decay_t<Container>::iterator>;
+    using const_iterator = EnumerateIterator<typename std::decay_t<Container>::const_iterator>;
+    using reverse_iterator = EnumerateIterator<typename std::decay_t<Container>::reverse_iterator>;
+    using const_reverse_iterator = EnumerateIterator<typename std::decay_t<Container>::const_reverse_iterator>;
+
+    iterator begin() { return {0, container.begin()}; }
+    iterator end() { return {container.size(), container.end()}; }
+    const_iterator cbegin() const { return {0, container.cbegin()}; }
+    const_iterator cend() const { return {container.size(), container.cend()}; }
+    reverse_iterator rbegin() { return {0, container.rbegin()}; }
+    reverse_iterator rend() { return {container.size(), container.rend()}; }
+    const_reverse_iterator crbegin() const { return {0, container.crbegin()}; }
+    const_reverse_iterator crend() const { return {container.size(), container.crend()}; }
+
     Container container;
+};
+
+/// Provide specialization for when the Container is const
+template <typename Container>
+struct Enumerate<const Container &> {
+    using iterator = EnumerateIterator<typename std::decay_t<Container>::const_iterator>;
+    using const_iterator = EnumerateIterator<typename std::decay_t<Container>::const_iterator>;
+    using reverse_iterator = EnumerateIterator<typename std::decay_t<Container>::const_reverse_iterator>;
+    using const_reverse_iterator = EnumerateIterator<typename std::decay_t<Container>::const_reverse_iterator>;
+
+    iterator begin() const { return {0, container.begin()}; }
+    iterator end() const { return {container.size(), container.end()}; }
+    const_iterator cbegin() const { return {0, container.cbegin()}; }
+    const_iterator cend() const { return {container.size(), container.cend()}; }
+    reverse_iterator rbegin() const { return {0, container.crbegin()}; }
+    reverse_iterator rend() const { return {container.size(), container.crend()}; }
+    const_reverse_iterator crbegin() const { return {0, container.crbegin()}; }
+    const_reverse_iterator crend() const { return {container.size(), container.crend()}; }
+
+    const Container &container;
 };
 }  // namespace detail
 
@@ -43,7 +73,7 @@ struct EnumerateCopy {
 /// NOTE: These will take const references as well, since the Container will be a const Container
 ///
 template <typename Container>
-detail::Enumerate<Container> enumerate(Container &container) {
+detail::Enumerate<Container &> enumerate(Container &container) {
     return {container};
 }
 template <typename Container>
@@ -51,7 +81,7 @@ detail::Enumerate<Container> enumerate(Container &&container) {
     return {container};
 }
 template <typename Container>
-detail::EnumerateCopy<Container> enumerate_copy(const Container &container) {
+detail::Enumerate<Container> enumerate_copy(const Container &container) {
     return {container};
 }
 }  // namespace it
